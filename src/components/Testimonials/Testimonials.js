9379aa0dd01b4157a1029976938f28c9087912ad
srikanthS64 <sri64srikanth@gmail.com>
2023-07-05 00:54:01 +0530

    new update

---
+++ src/components/Testimonials/Testimonials.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import"./testimonials.css"
+
+
+const Testimonials = () => {
+  return (
+    <div className='Testimonials'>
+      <div className='left-t'>
+        <span>TESTIMONIALS</span>
+        <span className='stroke-text'>WHAT THEY</span>
+        <span>SAY ABOUT US</span>
+      </div>
+      <div className='right-t'></div>
+    </div>
+  );
+};
+
+export default Testimonials;
