b1f00015333f8809558c6dc6e98f4b851053719e
srikanthS64 <sri64srikanth@gmail.com>
2023-07-04 11:19:27 +0530

    header left update

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import "./hero.css";
 import Header from '../Header/Header';
+import hero_image from "../../assets/hero_image";
+import hero_image_back from "../../assets/hero_image_back.png";
+import heart from "../../assets/heart.png";
+import calories from "../../assets/calories.png";
 
 
 function Hero() {
@@ -10,12 +14,46 @@ function Hero() {
         <div className='left-s'>
             <Header />
 
+            {/* the best ad */}
             <div className='the-best-ad'>
-                <div>
-                    <span>The Best Fitness Club In The Town</span>
-                </div>
+                <div></div>
+                <span>The Best Fitness Club In The Town</span>
             </div>
-        </div>
+
+            {/* Hero Heading*/}
+            <div className='hero-text'>
+              <div>
+                <span className='stroke-text'>shape </span>
+                <span>Your</span>
+              </div>
+              <div><span>Ideal Body</span></div>
+              <div> <span>In here we will help you to shape and build your ideal body and live up your life to fullest</span>
+              </div>
+            </div>
+
+             {/*figures */}
+              <div className='figures'>
+              <div>
+                <span>+ 140</span>
+                <span>EXPERT COACHES</span>
+              </div>
+              <div>
+                <span>+ 978</span>
+                <span>MEMBERS JOINED</span>
+              </div>
+              <div>
+               <span>+ 50</span>
+               <span>FITNESS PROGRAMS</span>
+              </div>
+             </div>
+
+             {/* HERO-BUTTON */}
+
+             <div className='hero-btn'>
+              <button className='btn'>Get Started</button>
+              <button className='btn'>Learn More</button>
+             </div>
+          </div>
         <div className='right-s'>Right-side</div>
       </div>
     </div>
