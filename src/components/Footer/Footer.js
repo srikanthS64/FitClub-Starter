3eab7355daac523baeda4fb9a0f8a01d5756ce81
srikanthS64 <sri64srikanth@gmail.com>
2023-07-05 12:23:55 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './footer.css';
-
+import Github from "../../assets/github.png";
+import Instagram from "../../assets/instagram.png";
+import LinkedIn from "../../assets/linkedin.png";
+import logo from "../../assets/logo.png";
 
 const Footer = () => {
   return (
     <div className='footer-container'>
       <hr/>
-      <div></div>
+      <div className='footer'> 
+        <div className='social-link'>
+            <img src={Github} alt=''/>
+            <img src={Instagram} alt=''/>
+            <img src={LinkedIn} alt=''/>
+        </div>
+        <div className='logo-f'>
+            <img src={logo} alt=''/>
+        </div>
+      </div>
     </div>
   );
 };
