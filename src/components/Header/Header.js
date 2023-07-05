2de27eb07394113e57e7629d50722bedddbfb461
srikanthS64 <sri64srikanth@gmail.com>
2023-07-05 16:12:18 +0530

    header update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,20 +2,37 @@ import React from 'react';
 import "./header.css"; 
 import logo from "../../assets/logo.png";
 import Bars from "../../assets/bars.png"
-
+import { useState } from 'react';
+import {link} from "react-scroll";
 
 function Header() {
+
+const mobile = window.innerWidth<=768 ? true: false;
+const [menuOpened, setMenuopened] = useState(false)
+
   return (
     <div className='Header'>
       <img className='logo' src={logo} alt=''/>
 
+      {(menuOpened=== false && mobile===true)? (
+        <div style={{
+          backgroundColor: "var(--appColor)",
+          padding: "0.5rem",
+          borderRadius: "5px",
+        }}
+        onClick={() => setMenuopened(true)}
+        >
+          <img src={Bars} alt='' style={{width:'1.5rem', height:'1.5rem'}}/>
+        </div>
+      ):(
       <ul className='header-menu'>
-        <li>Home</li>
-        <li>Programs</li>
-        <li>Why us</li>
-        <li>Plans</li>
-        <li>Testimonials</li>
+        <li onClick={() => setMenuopened(false)}>Home</li>
+        <li onClick={() => setMenuopened(false)}>Programs</li>
+        <li onClick={() => setMenuopened(false)}>Why us</li>
+        <li onClick={() => setMenuopened(false)}>Plans</li>
+        <li onClick={() => setMenuopened(false)}>Testimonials</li>
       </ul>
+      )}
     </div>
   );
 };
